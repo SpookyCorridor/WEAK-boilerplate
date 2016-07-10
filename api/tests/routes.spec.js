241568282cf9d601e83768bbabd2e0798bd276d1
dylan cairns <invision33@gmail.com>
2016-07-10 00:40:12 -0500

    remove unneccessry stuff from other repo

--- api/tests/routes.spec.js
+++ api/tests/routes.spec.js
@@ -5,37 +5,41 @@ const express = require('express');
 const expect = require('chai').expect;
 const server = supertest.agent('http://localhost:3000'); 
 
-describe('/api/contacts/', function(){
+/**
+ * EXAMPLE TEST with supretest 
+ */
 
-	describe('get', function(){
-		it ('should return json', function(done) {
-			server
-				.get('/api/contacts')
-                .expect(200)
-				.end(function(err,res){
-					if (err) return done(err); 
-					expect('Content-Type', /json/); 
-					done(); 
-				}); 
-        });
-	}); 
+// describe('/api/contacts/', function(){
+
+// 	describe('get', function(){
+// 		it ('should return json', function(done) {
+// 			server
+// 				.get('/api/contacts')
+//                 .expect(200)
+// 				.end(function(err,res){
+// 					if (err) return done(err); 
+// 					expect('Content-Type', /json/); 
+// 					done(); 
+// 				}); 
+//         });
+// 	}); 
     
-    describe('post', function(){
-        it('should return json', function(done) {
-            server
-                .post('/api/contacts')
-                .set('Accept', 'application/json')
-                .send(
-                    {'id': 'friend',
-                     'name': 'tester',
-                     'age': '21',
-                     'address': '612 4th avenue' })
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end(function(err,res) {
-                    expect(res.body._id).to.exist; 
-                    done(); 
-                });
-        });
-    });
-});
+//     describe('post', function(){
+//         it('should return json', function(done) {
+//             server
+//                 .post('/api/contacts')
+//                 .set('Accept', 'application/json')
+//                 .send(
+//                     {'id': 'friend',
+//                      'name': 'tester',
+//                      'age': '21',
+//                      'address': '612 4th avenue' })
+//                 .expect('Content-Type', /json/)
+//                 .expect(200)
+//                 .end(function(err,res) {
+//                     expect(res.body._id).to.exist; 
+//                     done(); 
+//                 });
+//         });
+//     });
+// });
